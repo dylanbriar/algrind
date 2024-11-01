0543d78acdd83b9becf0a52acc3ff718d52a77b7
Dylan Compton <dylancomptontx@gmail.com>
2024-10-31 19:40:17 -0500

    added buy/sell

---
+++ week-1/js/buy-sell-stock.js
@@ -0,0 +1,20 @@
+/* difficult to go line by line here, but here's what's up
+* you only need to keep track of a minimum, and the best difference you've found so far
+* if you find a lower minimum, that's more profit
+* unless there's a greater difference between the former minimum and a higher number behind the lower minimum
+* which we already recorded, so we won't lost it until we find a greater one. 
+* so there's two pointers: one for the minimum, one for the current number being observed
+*/
+function maxProfit (prices) {
+  let maxDif = 0;
+  let min = 0;
+  for (let i = 1; i < prices.length; i++){
+      if (prices[i] - prices[min] > maxDif){
+          maxDif = prices[i] - prices[min];
+      }
+      if (prices[i] < prices[min]){
+          min = i; 
+      }
+  }
+  return maxDif;
+};
