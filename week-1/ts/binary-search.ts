61400806e711ac73c470e69914fc8fcfb54566bd
Dylan Briar <142957940+dylanbriar@users.noreply.github.com>
2025-01-28 22:04:23 -0600

    Create binary-search.ts

---
+++ week-1/ts/binary-search.ts
@@ -0,0 +1,5 @@
+// given an array of integers nums, which is Sorted in ascending order, and an integer Target, write a function to search target in nums. if target exists, then return its index. otherwise, return -1.
+
+function binSearch(nums: Number[], tgt: Number): Number{
+  if (nums.length < 1) throw new Error('No values to search');
+  let right: Number = nums.length - 1;
