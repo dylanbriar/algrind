de0b8af7bade4e09250add574389b7ad99e99a11
Dylan Briar <142957940+dylanbriar@users.noreply.github.com>
2025-01-29 23:41:08 -0600

    Create bin-search.js needs conversion, do in vscode

---
+++ week-1/js/bin-search.js
@@ -0,0 +1,23 @@
+// given an array of integers nums, which is Sorted in ascending order, and an integer Target, write a function to search target in nums. if target exists, then return its index. otherwise, return -1.
+
+function binSearch (nums, tgt) {
+  if (nums.length < 1) throw new Error('No values to search');
+  let right: Number = nums.length - 1;
+  if (nums[right] === tgt) return right;
+  let left: Number = 0;
+  if (nums[left] === tgt) return left;
+  let mid: Number = Math.floor(right/2);
+  if (nums[mid] === tgt) return mid;
+  while (right > left){
+    if (nums[mid] > tgt){
+      right = mid; 
+      mid = left + Math.floor((right-left)/2);
+    }
+    else if (nums[mid] < tgt){
+      left = mid;
+      mid = left + Math.floor((right-left)/2);
+    }
+    else return mid;
+  }
+  return -1;
+}
